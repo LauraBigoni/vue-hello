49f42102075e0b7660226203162a532d5b51bf9e
Laura Bigoni <laurabigoni.92@gmail.com>
2022-01-26 15:37:23 +0100

    little style changes

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ Aggiungere alla pagina un’immagine, presa anch’essa da un data. */
 const app = new Vue({
     el: '#root',
     data: {
-        message: 'Vue Hello!',
+        title: 'Vue Hello!',
         image: 'vuehello.png',
     },
     methods: {
