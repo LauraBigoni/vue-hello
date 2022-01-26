1935c85229274850a6125669ef02e70b1e9ec1d8
Laura Bigoni <laurabigoni.92@gmail.com>
2022-01-26 14:48:07 +0100

    add files

---
+++ js/script.js
@@ -0,0 +1,18 @@
+console.log('Vue ok' , Vue);
+Vue.config.devtools = true;
+
+/* Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data. */
+
+// # creo un array Vue collegato al DOM
+const app = new Vue({
+    el: '#root',
+    data: {
+
+    },
+    methods: {
+
+    },
+});
